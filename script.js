4327ab73c0aff20fe444aa221d84be0955cd5388
Ajaay-Vivek <ajaayvivek14@gmail.com>
2026-07-30 20:15:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const startBtn = document.getElementById("startBtn");
 
 startBtn.addEventListener("click", () => {
-    const password = prompt("Enter the secret password:");
+    const password = prompt("When did i first IT for the first time - date:");
 
-    if (password === "secret123") {
+    if (password === "120726") {
         document.querySelector(".container").innerHTML = `
             <h1>🎉 Access Granted!</h1>
             <p>Welcome! Your video will appear below.</p>
@@ -11,12 +11,12 @@ startBtn.addEventListener("click", () => {
             <iframe
                 width="100%"
                 height="350"
-                src="YOUR_GOOGLE_DRIVE_EMBED_LINK"
+                src="https://drive.google.com/file/d/1_8Jc5kd5J4eKqXpdPz1nJru68K9lyJAw/view?usp=drive_link"
                 allow="autoplay"
                 allowfullscreen>
             </iframe>
         `;
     } else {
-        alert("❌ Incorrect password. Try again!");
+        alert("❌ Thappu po, thirupi try karo lol");
     }
 });
